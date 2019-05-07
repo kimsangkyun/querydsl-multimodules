71765a2b166358ea348cbd879f6d767a9847d87a
spring-cloud-config <ksk1004zz@gmail.com>
2019-05-07 23:09:11 +0900

    no message

--- api/src/main/java/com/musinsa/api/Controller.java
+++ api/src/main/java/com/musinsa/api/Controller.java
@@ -1,9 +1,10 @@
 package com.musinsa.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.HashMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
@@ -14,8 +15,17 @@ public class Controller {
         HashMap<String, String> hashMap = new HashMap<String, String>();
         hashMap.put("test", "test");
 
-
         return hashMap;
+    }
+
+    @GetMapping(value = "/cont")
+    public void test1(MemberAuthInfo memberIdV1) {
+        System.out.println("memberIdV1 : " + memberIdV1);
+    }
+
+    @PostMapping(value = "/ser")
+    public void test2(@RequestBody MemberAuthInfo memberIdV1) {
+        System.out.println("memberIdV1 : " + memberIdV1);
     }
 
 }
---
+++ api/src/main/java/com/musinsa/api/MemberAuthInfo.java
@@ -0,0 +1,13 @@
+package com.musinsa.api;
+
+public class MemberAuthInfo {
+    private long uid;
+    private String id;
+    private String password;
+    private Long auth;
+    private String authTempKey;
+    private String email;
+    private String phone;
+    private String mobile;
+}
+
